1abce65c94aa72e6715602ed4fea6411c9ffd522
pholarwalk <pholarwalk>
2020-05-07 13:49:03 +0100

    oluwasegunfolarinjoseph@gmail.com

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2> OLUWASEGUN IS HERE</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
